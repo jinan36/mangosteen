53430a93c6783f9bc57a9a4f37d6be177319294a
jinan36 <a492073467@gmail.com>
2022-09-26 10:58:57 +0800

    ValidationCodeController

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_024651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "validation_codes", force: :cascade do |t|
+    t.string "email"
+    t.integer "kind", default: 1, null: false
+    t.string "code", limit: 100
+    t.datetime "used_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
