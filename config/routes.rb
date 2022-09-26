8cbd5a8b0bbe1f6d5545757cb837d3ed29903da8
jinan36 <a492073467@gmail.com>
2022-09-26 10:45:42 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources :items, :tags
+    end
+  end
 end
