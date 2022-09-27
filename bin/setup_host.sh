377ab1c8a483256736cd9e754b670331ec675507
jinan36 <a492073467@gmail.com>
2022-09-27 11:21:57 +0800

    优化部署脚本、配置生产环境数据库信息

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -10,5 +10,5 @@ if [ "$(docker ps -aq -f name=^mangosteen-prod$)" ]; then
   docker rm -f $container_name
 fi
 echo 'docker run ...'
-docker run -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+docker run -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY --name=$container_name mangosteen:$version
 echo 'DONE!'
