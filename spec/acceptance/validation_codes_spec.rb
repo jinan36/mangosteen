2989296ee6c662bd0487ba4ad8c18578ef7280f4
jinan36 <a492073467@gmail.com>
2022-11-23 17:11:51 +0800

    新的测试用例以及新的文档

---
+++ spec/acceptance/validation_codes_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+require "rspec_api_documentation/dsl"
+
+resource "验证码" do
+  post "api/v1/validation_codes" do
+    # This is manual way to describe complex parameters
+    parameter :email, type: :string
+
+    let(:email) { "test@gmail.com" }
+
+    example "发送验证码" do
+      do_request
+
+      expect(status).to eq 200
+    end
+  end
+end
