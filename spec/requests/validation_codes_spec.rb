0cb5879b99abe5ec99b625161040dd0531552f17
jinan36 <a492073467@gmail.com>
2022-11-24 11:50:43 +0800

    文档只生成成功的情况，剩下走 request test

--- spec/requests/validation_codes_spec.rb
+++ spec/requests/validation_codes_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe "ValidationCodes", type: :request do
       post "/api/v1/validation_codes", params: {email: "a492073467@gmail.com"}
       expect(response).to have_http_status 200
     end
+    it "频繁发送会返回 429" do
+      post "/api/v1/validation_codes", params: {email: "a492073467@gmail.com"}
+      expect(response).to have_http_status 200
+      post "/api/v1/validation_codes", params: {email: "a492073467@gmail.com"}
+      expect(response).to have_http_status 429
+    end
   end
 end
