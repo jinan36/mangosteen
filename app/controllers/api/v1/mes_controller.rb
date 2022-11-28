5f82b94f77ab576fef7198595bf81e8263ed07cd
jinan36 <a492073467@gmail.com>
2022-11-28 11:44:21 +0800

    处理异常情况

--- app/controllers/api/v1/mes_controller.rb
+++ app/controllers/api/v1/mes_controller.rb
@@ -1,12 +1,27 @@
 class Api::V1::MesController < ApplicationController
   def show
     authorization = request.headers["Authorization"]
-    token = authorization.split(" ")[1]
+    token = begin
+      authorization.split(" ")[1]
+    rescue
+      ""
+    end
 
-    payload = JWT.decode token, Rails.application.credentials.hmac_secret, true, {algorithm: "HS256"}
-    user_id = payload[0]["user_id"]
+    payload = begin
+      JWT.decode token, Rails.application.credentials.hmac_secret, true, {algorithm: "HS256"}
+    rescue
+      nil
+    end
+
+    return head 400 if payload.nil?
+
+    user_id = begin
+      payload[0]["user_id"]
+    rescue
+      nil
+    end
     user = User.find user_id
-
+    return head 404 if user.nil?
     render status: 200, json: {
       resource: user
     }
