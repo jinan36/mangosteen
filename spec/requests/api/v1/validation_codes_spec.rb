a67e8fc9a4b45cf4db219172a03b4fbbbe738713
jinan36 <a492073467@gmail.com>
2022-11-28 11:10:11 +0800

    测试位置写反了，调整一下

--- spec/requests/api/v1/validation_codes_spec.rb
+++ spec/requests/api/v1/validation_codes_spec.rb
@@ -1,17 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "ValidationCodes", type: :request do
-  describe "会话" do
-    it "登录" do
-      User.create email: "a492073467@gmail.com"
-      post "/api/v1/session", params: {email: "a492073467@gmail.com", code: "123456"}
+  describe "验证码" do
+    it "频繁发送会返回 429" do
+      post "/api/v1/validation_codes", params: {email: "a492073467@gmail.com"}
       expect(response).to have_http_status 200
-      json = JSON.parse response.body
-      expect(json["jwt"]).to be_a(String)
-    end
-    it "未注册" do
-      post "/api/v1/session", params: {email: "a492073467@gmail.com", code: "123456"}
-      expect(response).to have_http_status 404
+      post "/api/v1/validation_codes", params: {email: "a492073467@gmail.com"}
+      expect(response).to have_http_status 429
     end
   end
 end
