c16c66916feb7a3e90e37a916844413738784b51
jinan36 <a492073467@gmail.com>
2022-11-30 14:06:45 +0800

    feat: 创建标签，顺手改下之前的代码

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TagsController < ApplicationController
   def index
     current_user = User.find request.env["current_user_id"]
-    return render status: :not_found if current_user.nil?
+    return render status: :unauthorized if current_user.nil?
 
     tags = Tag.where(user_id: current_user.id)
     render json: {resources: tags.page(params[:page]), pager: {
@@ -9,5 +9,17 @@ class Api::V1::TagsController < ApplicationController
       per_page: Tag.default_per_page,
       count: tags.count
     }}
+  end
+
+  def create
+    current_user = User.find request.env["current_user_id"]
+    return render status: :unauthorized if current_user.nil?
+
+    tag = Tag.new name: params[:name], sign: params[:sign], user_id: current_user.id
+    if tag.save
+      render json: {resource: tag}, status: :ok
+    else
+      render json: {errors: tag.errors}, status: :unprocessable_entity
+    end
   end
 end
--- spec/requests/api/v1/tags_spec.rb
+++ spec/requests/api/v1/tags_spec.rb
@@ -25,4 +25,33 @@ RSpec.describe "Tags", type: :request do
       expect(json["resources"][0]["user_id"]).to eq user1.id
     end
   end
+  describe "创建标签" do
+    it "未登录创建" do
+      post "/api/v1/tags", params: {name: "tag", sign: "x"}
+      expect(response).to have_http_status 401
+    end
+    it "已登录创建" do
+      user = User.create email: "1@qq.com"
+      post "/api/v1/tags", params: {name: "tag", sign: "x"}, headers: user.generate_auth_header
+      expect(response).to have_http_status 200
+      json = JSON.parse response.body
+      expect(json["resource"]["user_id"]).to eq user.id
+      expect(json["resource"]["name"]).to eq "tag"
+      expect(json["resource"]["sign"]).to eq "x"
+    end
+    it "已登录创建，name 为空" do
+      user = User.create email: "1@qq.com"
+      post "/api/v1/tags", params: {sign: "x"}, headers: user.generate_auth_header
+      expect(response).to have_http_status 422
+      json = JSON.parse response.body
+      expect(json["errors"]["name"][0]).to eq "can't be blank"
+    end
+    it "已登录创建，sign 为空" do
+      user = User.create email: "1@qq.com"
+      post "/api/v1/tags", params: {name: "tag"}, headers: user.generate_auth_header
+      expect(response).to have_http_status 422
+      json = JSON.parse response.body
+      expect(json["errors"]["sign"][0]).to eq "can't be blank"
+    end
+  end
 end
