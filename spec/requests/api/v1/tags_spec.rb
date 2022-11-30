52662d6bf37fa83622b69dff7b370bd917b25b28
jinan36 <a492073467@gmail.com>
2022-11-30 11:18:16 +0800

    分页获取标签功能完成

--- spec/requests/api/v1/tags_spec.rb
+++ spec/requests/api/v1/tags_spec.rb
@@ -1,7 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Api::V1::Tags", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Tags", type: :request do
+  describe "获取标签" do
+    it "未登录获取" do
+      get "/api/v1/tags"
+      expect(response).to have_http_status 401
+    end
+    it "已登录获取" do
+      user1 = User.create email: "1@qq.com"
+      user2 = User.create email: "2@qq.com"
+      11.times { |i| Tag.create name: "tag#{i}", sign: "x", user_id: user2.id }
+      11.times { |i| Tag.create name: "tag#{i}", sign: "x", user_id: user1.id }
+
+      get "/api/v1/tags", headers: user1.generate_auth_header
+      expect(response).to have_http_status 200
+      json = JSON.parse response.body
+      expect(json["resources"].size).to eq 10
+      expect(json["resources"][0]["user_id"]).to eq user1.id
+
+      get "/api/v1/tags", headers: user1.generate_auth_header, params: {page: 2}
+      expect(response).to have_http_status 200
+      json = JSON.parse response.body
+      expect(json["resources"].size).to eq 1
+      expect(json["resources"][0]["user_id"]).to eq user1.id
+    end
   end
 end
